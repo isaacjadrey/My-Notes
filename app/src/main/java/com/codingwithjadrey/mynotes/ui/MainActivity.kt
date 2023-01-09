e20b72b20f8522d7e219a093c799a68a1682db96
Isaac Jadrey <jadreyisaac@gmail.com>
2023-01-09 16:48:46 +0300

    updated nav_graph and MainActivity to handle nav destinations

--- app/src/main/java/com/codingwithjadrey/mynotes/ui/MainActivity.kt
+++ app/src/main/java/com/codingwithjadrey/mynotes/ui/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.codingwithjadrey.mynotes.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.codingwithjadrey.mynotes.R
+import com.codingwithjadrey.mynotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.apply {
+            bottomNav.setupWithNavController(navController)
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                bottomNav.visibility = when (destination.id) {
+                    R.id.noteListFragment -> View.VISIBLE
+                    R.id.quoteListFragment -> View.VISIBLE
+                    R.id.todoListFragment -> View.VISIBLE
+                    else -> View.GONE
+                }
+            }
+        }
     }
 }
