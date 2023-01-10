7ea42d2febcb10ad765cc7026473b9d632234897
Isaac Jadrey <jadreyisaac@gmail.com>
2023-01-10 21:31:40 +0300

    added util package and created a Constant object

---
+++ app/src/main/java/com/codingwithjadrey/mynotes/util/Constant.kt
@@ -0,0 +1,9 @@
+package com.codingwithjadrey.mynotes.util
+
+object Constant {
+    const val NOTE_COLLECTION = "notes"
+    const val QUOTE_COLLECTION = "quotes"
+    const val TASK_COLLECTION = "tasks"
+    const val USER_COLLECTION = "users"
+    const val DATE = "date"
+}
