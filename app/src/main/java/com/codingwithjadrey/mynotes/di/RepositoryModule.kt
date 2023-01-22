f1a797a8c3e0eb0b1bb4eccc446e954c7f112132
Isaac Jadrey <jadreyisaac@gmail.com>
2023-01-22 17:42:23 +0300

    provided a dagger module for quote repository

--- app/src/main/java/com/codingwithjadrey/mynotes/di/RepositoryModule.kt
+++ app/src/main/java/com/codingwithjadrey/mynotes/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.codingwithjadrey.mynotes.di
 
 import com.codingwithjadrey.mynotes.repository.note.NoteRepository
 import com.codingwithjadrey.mynotes.repository.note.NoteRepositoryImp
+import com.codingwithjadrey.mynotes.repository.quote.QuoteRepository
+import com.codingwithjadrey.mynotes.repository.quote.QuoteRepositoryImp
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
@@ -17,5 +19,11 @@ object RepositoryModule {
     @Singleton
     fun provideNoteRepository(firestore: FirebaseFirestore): NoteRepository {
         return NoteRepositoryImp(firestore)
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuoteRepository(firestore: FirebaseFirestore): QuoteRepository {
+        return QuoteRepositoryImp(firestore)
     }
 }
